e6cf417e99cb3caa2c121bd4da69459bf4d61a26
juncwang <juncwang@live.com>
2019-03-27 14:04:32 +0800

    updata

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ mongoose.connect(MongoURI, {useNewUrlParser: true})
         console.log(err)
     })
 
+//设置允许跨域访问该服务.
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+//     res.header('Access-Control-Allow-Headers', 'Content-Type')
+//     res.header('Access-Control-Allow-Methods', '*')
+//     next();
+//   })
+
 // 使用自定义路由
 const users = require('./routes/api/users')
 const moments = require('./routes/api/moments')
