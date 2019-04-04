e74f16a1924293884c0e6dc520a48ca52593e96d
juncwang <juncwang@live.com>
2019-04-04 14:38:10 +0800

    updata

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const app = require('../../server')
 
 const multiparty = require('connect-multiparty')
 const multipartyMiddeware = multiparty()
@@ -10,12 +9,13 @@ const fs = require('fs');
 // app.use('/api/files', router)
 // app.use(multiparty({uploadDir: './updata/'}))
 
+
 router.get('/test', (req, res) => {
-    res.json({msg: 'heoo'})
+    res.json({ msg: 'heoo' })
 })
 
 router.post('/upload', multipartyMiddeware, (req, res) => {
-    console.log(req.files)
+    
 
     let filesData = req.files.file.path.split('\\')
     let outPath = filesData[filesData.length - 1]
@@ -25,10 +25,20 @@ router.post('/upload', multipartyMiddeware, (req, res) => {
 
     source.pipe(dest)
 
-    source.on('end', function() { fs.unlinkSync(req.files.file.path);});   //delete
-    source.on('error', function(err) {  });
+    source.on('end', function () { fs.unlinkSync(req.files.file.path); });   //delete
+    source.on('error', function (err) { });
 
-    res.json({msg: 'success'})
+    res.json({uri: 'http://192.168.78.113:5000/api/files/img/' + outPath})
+})
+
+router.get('/downdata/:fileName', (req, res) => {
+    res.download('./updata/' + req.params.fileName)
+    // res.json({msg: 'hello world'})
+})
+
+router.get('/img/:fileName', (req, res) => {
+    const rs = fs.createReadStream('./updata/' + req.params.fileName);//获取图片的文件名
+    rs.pipe(res);
 })
 
 module.exports = router
